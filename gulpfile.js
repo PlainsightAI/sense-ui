06dd1a4e3e265902d009ceae2bce8cc67bdf8f1d
Kaare Larsen <me@kaareal.com>
2017-08-21 13:53:29 +0200

    bower -> npm

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ var paths = {
   // Sass will check these folders for files when you use @import.
   sass: [
     'client/assets/scss',
-    'bower_components/foundation-apps/scss'
+    'node_modules/foundation-apps/scss'
   ],
   // These files include Foundation for Apps and its dependencies
   foundationJS: [
